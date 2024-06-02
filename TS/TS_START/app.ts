d84aa5f67b24e8eba4cd206b203bd49b69b23cbd
beliyandr <beliyandr@gmail.com>
2024-06-02 15:45:44 +0300

    TS

---
+++ TS/TS_START/app.ts
@@ -0,0 +1,19 @@
+// function add(x: number, y: number): void {
+//   return;
+// }
+
+// let res = add(1, 2);
+
+// console.log(res);
+
+// type Callback = (x: number, y: number) => number;
+
+// const multi = (x: number, y: number): number => {
+//   return x + y;
+// };
+
+// const subtrack: Callback = (x, y) => {
+//   return x - y;
+// };
+
+
